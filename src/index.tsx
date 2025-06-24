ba7441c8a6101d4428207b6c43c50299e197a0d8
Kszlufik <ataner31@hotmail.com>
2025-06-24 21:01:32 +0200

    Full review page.

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
-import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
+import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
+import MovieReviewPage from "./pages/movieReviewPage"; // NEW
 
 const App = () => {
   return (
@@ -17,9 +18,10 @@ const App = () => {
         </li>
       </ul>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
-        <Route path="/" element={<HomePage />} />
+        <Route path="/reviews/:id" element={<MovieReviewPage />} /> {/*slight improvment as i could not display the movies due to white page*/}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
@@ -29,6 +31,5 @@ const App = () => {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
-
+  </React.StrictMode>
+);
